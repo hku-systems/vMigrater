57a3827ff79bd60d00e4f972ebef5e048c06cfb4
Weiwei Jia <wj47@njit.edu>
2017-12-08 00:25:16 -0500

    fix hdfs2 script

--- macro_benchmarks/bench8/hdfs2.sh
+++ macro_benchmarks/bench8/hdfs2.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-process_id=`/bin/ps -aux | grep "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java" | grep -v "grep" | grep -v "bash" | awk '{print $2}'`
-echo "hdfs main thread id: $process_id"
-
-cd /proc/$process_id/task
-#search_dir=/proc/$process_id/task
-
-for entry in *
-do
-	sudo taskset -pc $1 $entry
+while true; do
+	process_id=`/bin/ps -aux | grep "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java" | grep -v "grep" | grep -v "bash" | awk '{print $2}'`
+	echo "hdfs main thread id: $process_id"
+	
+	cd /proc/$process_id/task
+	#search_dir=/proc/$process_id/task
+	
+	for entry in *
+	do
+		sudo taskset -pc $1 $entry
+	done
 done
-
-
-
