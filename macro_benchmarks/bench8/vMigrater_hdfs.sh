77e8cf03d72521312364c791adb2bad179b6b83b
Weiwei Jia <wj47@njit.edu>
2017-12-08 04:41:20 -0500

    revise solution for hdfs macrbench

--- macro_benchmarks/bench8/vMigrater_hdfs.sh
+++ macro_benchmarks/bench8/vMigrater_hdfs.sh
@@ -1,45 +1,37 @@
 #!/bin/bash
 
+#while true; do
 process_id=`/bin/ps -aux | grep "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java" | grep -v "grep" | grep -v "bash" | awk '{print $2}'`
-echo $process_id
+echo "hdfs main thread id: $process_id"
+	
+cd /proc/$process_id/task/
+#search_dir=/proc/$process_id/task
+#files=(*)
 
-#if [ "$1" == "1" ]
-#then
-#	#echo "sysbench main process id is $process_id1"
-#	#echo "sysbench I/O process id is $process_id"
-#	let process_id1=($process_id + 1)
-#	let process_id2=($process_id1 + 1)
-#	./main $2
-#elif [ "$1" == "2" ]
-#then
-#	let process_id1=($process_id + 1)
-#	let process_id2=($process_id1 + 1)
-#	let process_id3=($process_id2 + 1)
-#	./main $2 $3
-#elif [ "$1" == "4" ]
-#then
-#	let process_id1=($process_id + 1)
-#	let process_id2=($process_id1 + 1)
-#	let process_id3=($process_id2 + 1)
-#	let process_id4=($process_id3 + 1)
-#	let process_id5=($process_id4 + 1)
-#	./main $process_id1 $process_id2 $process_id3 $process_id4 $process_id5
-#elif [ "$1" == "8" ]
-#then
-#	let process_id1=($process_id + 1)
-#	let process_id2=($process_id1 + 1)
-#	let process_id3=($process_id2 + 1)
-#	let process_id4=($process_id3 + 1)
-#	let process_id5=($process_id4 + 1)
-#	let process_id6=($process_id5 + 1)
-#	let process_id7=($process_id6 + 1)
-#	let process_id8=($process_id7 + 1)
-#	./main $process_id1 $process_id2 $process_id3 $process_id4 $process_id5 $process_id6 $process_id7 $process_id8
-#fi
-#process_id1=`echo $process_id | awk -F' ' '{print $1}'`
-#process_id2=`echo $process_id | awk -F' ' '{print $2}'`
+#files=*
+echo *
 
-#echo $process_id1
-#echo $process_id2
+./main *
 
+#total=${#files[*]}
 
+#echo "there are $total tasks under main thread"
+
+#for (( i=0; i<=$(( $total -1 )); i++ ))
+#do 
+#	echo -n "${files[$i]} "
+#done
+
+#echo
+
+#echo ${files[1]}
+	
+#for entry in *
+#do
+#	#sudo taskset -pc $1 $entry
+#	$i=$entry
+#	i=i+1
+#done
+#
+#for (( i=0; i<=$(( $total -1 )); i++ ))
+#done
