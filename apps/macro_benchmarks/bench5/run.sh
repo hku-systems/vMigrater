0a4cbee652eec1087236c36505ec5ff5896e3d00
Weiwei Jia <wj47@njit.edu>
2017-12-20 09:13:27 -0500

    merge macrobench 5 into eval framework

---
+++ apps/macro_benchmarks/bench5/run.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+BENCH5_DIR=/home/kvm1/vMigrater/apps/macro_benchmarks/bench5
+
+echo "This is macrobenchmark 5: $BENCH5_DIR"
+echo "vMigrater script dir is $1"
+echo "vMigrater results for macrobench 5 dir is $2"
+echo "vMigrater tools dir is $3"
+
+process_id1=`/bin/ps -aux | grep "nginx:" | grep -v "grep" | grep -v "bash" | awk '{print $2}' | head -n1`
+process_id2=`/bin/ps -aux | grep "nginx:" | grep -v "grep" | grep -v "bash" | awk '{print $2}' | head -n2`
+process_id3=`/bin/ps -aux | grep "nginx:" | grep -v "grep" | grep -v "bash" | awk '{print $2}' | head -n3`
+process_id4=`/bin/ps -aux | grep "nginx:" | grep -v "grep" | grep -v "bash" | awk '{print $2}' | head -n4`
+process_id5=`/bin/ps -aux | grep "nginx:" | grep -v "grep" | grep -v "bash" | awk '{print $2}' | head -n5`
+
+echo ">>>>>>>>>>>>>>>>>>>nginx, dedicated" > $2
+#FIXME: this should be done in server side
+sudo taskset -pc 1 $process_id1
+sudo taskset -pc 1 $process_id2
+sudo taskset -pc 1 $process_id3
+sudo taskset -pc 1 $process_id4
+sudo taskset -pc 1 $process_id5
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/nginx2.sh 1 500 >> $2
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/nginx2.sh 1 500 >> $2
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/nginx2.sh 1 1000 >> $2
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/nginx2.sh 1 1000 >> $2
+
+echo ">>>>>>>>>>>>>>>>>>>nginx, shared" >> $2
+#FIXME: this should be done in server side
+sudo taskset -pc 5 $process_id1
+sudo taskset -pc 5 $process_id2
+sudo taskset -pc 5 $process_id3
+sudo taskset -pc 5 $process_id4
+sudo taskset -pc 5 $process_id5
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/nginx2.sh 5 500 >> $2
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/nginx2.sh 5 500 >> $2
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/nginx2.sh 5 1000 >> $2
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/nginx2.sh 5 1000 >> $2
+
+echo ">>>>>>>>>>>>>>>>>>>nginx, shared with vMigrater" >> $2
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/vMigrater_nginx.sh &
+$BENCH5_DIR/web_server/nginx2.sh 5 500 >> $2
+killall -9 main
+killall -9 vMigrater_nginx.sh
+
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/vMigrater_nginx.sh &
+$BENCH5_DIR/web_server/nginx2.sh 5 500 >> $2
+killall -9 main
+killall -9 vMigrater_nginx.sh
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/vMigrater_nginx.sh &
+$BENCH5_DIR/web_server/nginx2.sh 5 1000 >> $2
+killall -9 main
+killall -9 vMigrater_nginx.sh
+
+$1/umount.sh
+$1/mount.sh
+$3/flush
+$BENCH5_DIR/web_server/vMigrater_nginx.sh &
+$BENCH5_DIR/web_server/nginx2.sh 5 1000 >> $2
+killall -9 main
+killall -9 vMigrater_nginx.sh
--- apps/macro_benchmarks/bench5/web_server/nginx2.sh
+++ apps/macro_benchmarks/bench5/web_server/nginx2.sh
@@ -3,7 +3,10 @@
 #if [ "$1" = "1" ]
 #then
 	# 1st run on shared vCPU
-	echo "There are $1 users"
+
+echo "nginx, this is on vCPU $1 with $2 clients/users"
+#FIXME: IP should be not the server IP
+IP_ADDR=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 	#cd /home/kvm1/vMigrater/macro_benchmarks/bench3/sysbench
 	#./umount.sh
 	#./mount.sh
@@ -12,12 +15,12 @@
 	#/usr/bin/sysbench --test=fileio --file-total-size=10G prepare
 	#sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test --mysql-user=root --mysql-password=123 prepare
 	#./flush
-	echo "============> siege start to run===============>"
+echo "============> siege start to run===============>"
 	#/usr/bin/taskset -c 6 /usr/bin/sysbench --test=fileio --max-time=30 --max-requests=0 --file-total-size=8GB --num-threads=$2 --file-test-mode=rndrw run
 	#/usr/bin/taskset -c $1 /usr/bin/sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test --mysql-user=root --mysql-password=123 --max-time=60 --oltp-read-only=on --max-requests=0 --num-threads=$2 run
 	#client 1000 users and each send a total of 
-	/usr/bin/taskset -c 1 /usr/bin/siege --quiet --concurrent=$1 --time=60s --log=./siege.log 192.168.122.95
-	echo "============> siege end===============>"
+/usr/bin/taskset -c $1 /usr/bin/siege --quiet --concurrent=$2 --time=60s --log=./siege.log $IP_ADDR
+echo "============> siege end===============>"
 	#sysbench --test=oltp --mysql-db=test --mysql-user=root --mysql-password=123 cleanup
 	#/usr/bin/sysbench --test=fileio --file-total-size=10G cleanup
 	#/usr/bin/taskset -c 6 /usr/bin/dbench -c /usr/share/dbench/client.txt -D /home/kvm1/sda3/ -t 30 $2
