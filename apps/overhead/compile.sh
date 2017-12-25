a31cea1a28376d224f9b83bf327ea8ea49b12504
Weiwei Jia <wj47@njit.edu>
2017-12-25 16:07:41 -0500

    add getting vcpu time slice tool

--- apps/overhead/compile.sh
+++ apps/overhead/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 gcc cpu.c debug.c -o cpu -lpthread -lglib-2.0
+gcc get_vcpu_ts.c debug.c -o get_vcpu_ts -lpthread -lglib-2.0
 gcc io_original.c debug.c -o io_original -lpthread -lglib-2.0
 gcc io_original_nothinktime.c debug.c -o io_original_nothinktime -lpthread -lglib-2.0
 gcc io_original_daemon.c debug.c -o io_original_daemon -lpthread -lglib-2.0
