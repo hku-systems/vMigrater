7f3d31c9c22adb8f1060c7484c9e8341ba5b7a01
Weiwei Jia <wj47@njit.edu>
2018-01-11 10:25:37 -0500

    tune hdfs

--- apps/macro_benchmarks/bench8/datanode.sh
+++ apps/macro_benchmarks/bench8/datanode.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #while true; do
-	process_id=`/bin/ps -aux | grep "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java" | grep -v "grep" | grep -v "bash" | awk '{print $2}'`
-	echo "hdfs main thread id: $process_id"
+	#process_id=`/bin/ps -aux | grep "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java" | grep -v "grep" | grep -v "bash" | awk '{print $2}'`
+	echo "hdfs main thread id: $1"
 	
-	cd /proc/$process_id/task
+	cd /proc/$2/task
 	#search_dir=/proc/$process_id/task
 	
 	for entry in *
