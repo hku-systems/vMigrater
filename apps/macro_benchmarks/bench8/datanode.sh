e8bf092a6b8b082743e95f588740f7f598532ef4
Weiwei Jia <wj47@njit.edu>
2018-01-11 22:34:37 -0500

    tune nginx

--- apps/macro_benchmarks/bench8/datanode.sh
+++ apps/macro_benchmarks/bench8/datanode.sh
@@ -2,7 +2,7 @@
 
 #while true; do
 	#process_id=`/bin/ps -aux | grep "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java" | grep -v "grep" | grep -v "bash" | awk '{print $2}'`
-	echo "hdfs main thread id: $1"
+	echo "hdfs main thread id: $2"
 	
 	cd /proc/$2/task
 	#search_dir=/proc/$process_id/task
